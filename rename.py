7a4fd0073c620543d8568ed754776bc4e5bcd819
gram <git@orsinium.dev>
2023-07-17 12:41:40 +0200

    rename files

---
+++ rename.py
@@ -0,0 +1,22 @@
+from pathlib import Path
+
+import re
+import yaml
+
+REX_SLUG = re.compile(r'[a-zA-Z\-]{,20}')
+
+
+for dir_path in sorted(Path('content', 'posts').iterdir()):
+    for path in dir_path.iterdir():
+        text = path.read_text()
+        try:
+            int(path.stem[11:])
+        except ValueError:
+            continue
+        meta = text.split('---\n')[1]
+        title = str(yaml.safe_load(meta)['title'])
+        if not REX_SLUG.fullmatch(title):
+            continue
+        title = title.lower()
+        new_path = path.parent / f'{path.stem[:10]}-{title}.md'
+        path.rename(new_path)
