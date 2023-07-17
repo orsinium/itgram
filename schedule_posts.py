daeba455f7f3883e73b6b24b541f6d814910bd0b
gram <git@orsinium.dev>
2023-07-17 13:06:33 +0200

    do not schedule twice

--- schedule_posts.py
+++ schedule_posts.py
@@ -4,9 +4,20 @@ import os
 from pathlib import Path
 from random import randint
 from telethon import TelegramClient
+from telethon.types import Message
 
 # Remember to use your own values from my.telegram.org!
 client = TelegramClient('bot', os.environ['API_ID'], os.environ['API_HASH'])
+CHANNEL = 'itgram_channel'
+
+
+async def scheduled() -> list[date]:
+    result = []
+    message: Message
+    async for message in client.iter_messages(CHANNEL, scheduled=True):
+        assert message.date
+        result.append(message.date.date())
+    return result
 
 
 async def schedule_post(send_on: date, path: Path) -> None:
@@ -22,7 +33,7 @@ async def schedule_post(send_on: date, path: Path) -> None:
 
     print(f'scheduling {path.stem}')
     await client.send_message(
-        'itgram_channel',
+        CHANNEL,
         text,
         schedule=send_at,
         link_preview=False,
@@ -32,9 +43,13 @@ async def schedule_post(send_on: date, path: Path) -> None:
 
 async def main():
     async with client:
+        dates = await scheduled()
         for path in Path('content').glob('*/**/*.md'):
             send_on = date.fromisoformat(path.stem[:10])
             if send_on < date.today():
+                continue
+            if send_on in dates:
+                print(f'{path.stem} already scheduled on {send_on}')
                 continue
             await schedule_post(send_on, path)
 
