d02845de88ef767562a91315cfa8329ecb41d817
gram <git@orsinium.dev>
2023-07-17 09:54:53 +0200

    add title to more posts

--- add_title.py
+++ add_title.py
@@ -10,6 +10,26 @@ def get_links(text: str) -> list[str]:
 
 
 def detect_title(text: str) -> str | None:
+    return detect_title_from_url(text) or get_title_from_first_link_text(text)
+
+
+def get_title_from_first_link_text(text: str) -> str | None:
+    text = text.split('layout: post\n---')[-1]
+    text = text.strip()
+    if not text:
+        return None
+    if text[0] != '[':
+        text = text[1:]
+    text = text.strip()
+    if text[0] != '[':
+        return None
+    title = text.lstrip('[').split(']')[0]
+    if title[1] == ' ':
+        title = title[2:]
+    return f'"{title}"'
+
+
+def detect_title_from_url(text: str) -> str | None:
     links = get_links(text)
     if not links:
         return None
@@ -19,7 +39,7 @@ def detect_title(text: str) -> str | None:
     return link.rstrip('/').split('/')[-1]
 
 
-for dir_path in sorted(Path('posts').iterdir()):
+for dir_path in sorted(Path('content', 'posts').iterdir()):
     for path in dir_path.iterdir():
         text = path.read_text()
         if not REX_BAD_TITLE.findall(text):
